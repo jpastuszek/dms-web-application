328c71e313ab45e64251ab40dda1fe200df13fdf
Jakub Pastuszek <jpastuszek@gmail.com>
2012-04-24 19:41:41 +0200

    using Rack::Utils::HTTP_STATUS_CODES instead of harcoded messages

--- lib/dms-web-application/rack.rb
+++ lib/dms-web-application/rack.rb
@@ -73,52 +73,9 @@ module Rack
 			end
 
 			class Response
-				StatusMessage = {
-					100 => 'Continue',
-					101 => 'Switching Protocols',
-					200 => 'OK',
-					201 => 'Created',
-					202 => 'Accepted',
-					203 => 'Non-Authoritative Information',
-					204 => 'No Content',
-					205 => 'Reset Content',
-					206 => 'Partial Content',
-					300 => 'Multiple Choices',
-					301 => 'Moved Permanently',
-					302 => 'Found',
-					303 => 'See Other',
-					304 => 'Not Modified',
-					305 => 'Use Proxy',
-					307 => 'Temporary Redirect',
-					400 => 'Bad Request',
-					401 => 'Unauthorized',
-					402 => 'Payment Required',
-					403 => 'Forbidden',
-					404 => 'Not Found',
-					405 => 'Method Not Allowed',
-					406 => 'Not Acceptable',
-					407 => 'Proxy Authentication Required',
-					408 => 'Request Timeout',
-					409 => 'Conflict',
-					410 => 'Gone',
-					411 => 'Length Required',
-					412 => 'Precondition Failed',
-					413 => 'Request Entity Too Large',
-					414 => 'Request-URI Too Large',
-					415 => 'Unsupported Media Type',
-					416 => 'Request Range Not Satisfiable',
-					417 => 'Expectation Failed',
-					500 => 'Internal Server Error',
-					501 => 'Not Implemented',
-					502 => 'Bad Gateway',
-					503 => 'Service Unavailable',
-					504 => 'Gateway Timeout',
-					505 => 'HTTP Version Not Supported'
-				}
-
 				def self.header(uuid, conn_id, status, headers)
 					headers = headers.map{ |k, v| "#{k}: #{v}" }.join("\r\n")
-					self.new(uuid, conn_id, "HTTP/1.1 #{status.to_i} #{StatusMessage[status.to_i]}\r\n#{headers}\r\n\r\n")
+					self.new(uuid, conn_id, "HTTP/1.1 #{status.to_i} #{Rack::Utils::HTTP_STATUS_CODES[status.to_i]}\r\n#{headers}\r\n\r\n")
 				end
 
 				def self.body(uuid, conn_id, body)
