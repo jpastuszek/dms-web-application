7c03820f58200a386b7d5603b331fd237ef9026c
Jakub Pastuszek <jpastuszek@gmail.com>
2012-05-05 19:19:41 +0200

    handler unescapes URI

--- lib/dms-web-application/rack/handler/dms_mongrel2.rb
+++ lib/dms-web-application/rack/handler/dms_mongrel2.rb
@@ -61,8 +61,8 @@ module Rack
 							'rack.multiprocess' => true,
 							'rack.run_once' => false,
 							'REQUEST_METHOD' => headers.delete('METHOD'),
-							'SCRIPT_NAME' => script_name,
-							'PATH_INFO' => headers.delete('PATH')[script_name.length..-1], 
+							'SCRIPT_NAME' => Rack::Utils.unescape(script_name),
+							'PATH_INFO' => Rack::Utils.unescape(headers.delete('PATH')[script_name.length..-1]), 
 							'QUERY_STRING' => headers.delete('QUERY') || ''
 						}
 						@env['SERVER_NAME'], @env['SERVER_PORT'] = headers['host'].split(':', 2)
