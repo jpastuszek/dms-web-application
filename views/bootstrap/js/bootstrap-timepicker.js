4ca08a8ba57ca6549a7789706b33f312ce46ccbf
Jakub Pastuszek <jpastuszek@gmail.com>
2012-05-07 22:23:11 +0200

    an attempt to get now button working

--- views/bootstrap/js/bootstrap-timepicker.js
+++ views/bootstrap/js/bootstrap-timepicker.js
@@ -124,26 +124,35 @@
             this.minute = parseInt(timeArray[1].replace(/^0/, ''));
         }
 
+		, setCurrentTime: function() {
+			var dTime = new Date();
+			var hours = dTime.getHours();
+			var minutes = Math.floor(dTime.getMinutes() / this.minuteStep) * this.minuteStep;
+
+			var meridian = "AM";
+			if (hours === 0) {
+				hours = 12;
+			} else if (hours > 12) {
+				hours = hours - 12;
+				meridian = "PM";
+			} else {
+			   meridian = "AM";
+			}
+
+			this.hour = hours;
+			this.minute = minutes;
+			this.meridian = meridian;
+		}
+
+		, reset: function() {
+			this.setCurrentTime();
+			this.update();
+		}
+
         , setDefaultTime: function(defaultTime){
             if (defaultTime) {
                 if (defaultTime === 'current') {
-                    var dTime = new Date();
-                    var hours = dTime.getHours();
-                    var minutes = Math.floor(dTime.getMinutes() / this.minuteStep) * this.minuteStep;
-
-                    var meridian = "AM";
-                    if (hours === 0) {
-                        hours = 12;
-                    } else if (hours > 12) {
-                        hours = hours - 12;
-                        meridian = "PM";
-                    } else {
-                       meridian = "AM";
-                    }
-
-                    this.hour = hours;
-                    this.minute = minutes;
-                    this.meridian = meridian;
+					this.setCurrentTime();
                 } else {
                     this.setValues(defaultTime);
                 }
