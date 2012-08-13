3cdbeae150423b6cfabb79426a0e275506ca26f0
Jakub Pastuszek <jpastuszek@gmail.com>
2012-08-13 13:46:24 +0200

    numberpicker: addec copyrights

--- assets/js/bootstrap-numberpicker.js
+++ assets/js/bootstrap-numberpicker.js
@@ -1,3 +1,25 @@
+/* =========================================================
+ * bootstrap-numberpicker.js 
+ * =========================================================
+ * Copyright 2012 Jakub Pastuszek
+ *
+ * Based on bootstrap-timepicker by:
+ * Joris de Wit @joris_dewit
+ * Gilbert @mindeavor
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ========================================================= */
+
 !function($) {
 	var Numberpicker = function(element, options) {
 		this.options = $.extend({}, $.fn.numberpicker.defaults, options);
